d0a2c157edb1ce87acb778aa690bfcf84ab49090
Franzoso69 <rec.acq.gmo@gmail.com>
2022-03-25 01:22:10 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, FlexLayoutModule, MaterialModule, MatToolbarModule],
-  declarations: [AppComponent, HelloComponent, MyCoolComponent,],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    MatToolbarModule,
+  ],
+  declarations: [AppComponent, HelloComponent, MyCoolComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
