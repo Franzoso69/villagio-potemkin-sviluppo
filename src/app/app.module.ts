b5ad02685c8a4be84cc9f2e87d59e30eb55a3ad8
Franzoso69 <rec.acq.gmo@gmail.com>
2022-03-25 01:42:30 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule } from './material.module';
+
 
 
 @NgModule({
