0f99f76efd49fbc2ad8e7f8cd6ee0c9048e09e4c
Franzoso69 <rec.acq.gmo@gmail.com>
2022-03-26 12:58:46 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,27 +13,28 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
-import {  MatGridListModule} from '@angular/material/grid-list';
-import {  MatListModule} from '@angular/material/list';
-import {  MatMenuModule} from '@angular/material/menu';
-  /* import {MatNativeDateModule} from '@angular/material/native-date'; */
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+/* import {MatNativeDateModule} from '@angular/material/native-date'; */
 /*import{  MatPaginatorModule} from ' @angular/material/paginator'; */
-import{  MatProgressBarModule} from '@angular/material/progress-bar';
-import{  MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import{  MatSidenavModule} from '@angular/material/sidenav';
-  import {MatSliderModule} from '@angular/material/slider';
-import {  MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 /* import{  MatSnackBarModule} from ' @angular/material/snackbar'; */
-import{  MatSortModule } from '@angular/material/sort';
-  import {MatStepperModule} from '@angular/material/stepper';
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-  MatButtonModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatRippleModule,
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+/* import {  MatRippleModule} from '@angular/material/ripple'; */
+import {
   MatIconModule,
   MatCardModule,
   MatExpansionModule,
