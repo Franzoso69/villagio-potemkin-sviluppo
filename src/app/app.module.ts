38ea61a621b339707c6724d6f43ca6d8f2cc5087
Franzoso69 <rec.acq.gmo@gmail.com>
2022-03-26 19:22:17 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,12 +34,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 /* import {  MatRippleModule} from '@angular/material/ripple'; */
-import {
-  MatIconModule,
-  MatCardModule,
-  MatExpansionModule,
-  MatCheckboxModule,
-  MatSelectModule,
+import {  MatIconModule} from '@angular/material/icon';
+import {  MatCardModule} from '@angular/material/card';
+import { MatExpansionModule} from '@angular/material/expansion';
+import{  MatCheckboxModule} from '@angular/material/checkbox';
+import{
+    MatSelectModule} from '@angular/material/select';
   MatRadioModule,
   MatTableModule,
   MatDialogModule,
